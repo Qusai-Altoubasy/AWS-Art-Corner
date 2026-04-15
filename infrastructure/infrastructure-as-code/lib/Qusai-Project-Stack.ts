d9ff3c8837cce9cb550de945abede6c7e44d0b85
Qusai Altoubasy <qaltwbasy@gmail.com>
2026-04-16 02:33:02 +0300

    fix: networking file extension and link it to MainStack

--- infrastructure/infrastructure-as-code/lib/Qusai-Project-Stack.ts
+++ infrastructure/infrastructure-as-code/lib/Qusai-Project-Stack.ts
@@ -1,9 +1,12 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Networking } from '../constructs/networking';
 
 export class QusaiProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    
+
+    const networking = new Networking(this, 'NetworkingLayer');
+
   }
 }
